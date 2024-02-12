76634ffee31b055cb001d59dc788e647a5314ce8
Logan Halstead <ljhalstead16@gmail.com>
2024-02-12 10:16:33 -0600

    Start of project
    Project created

---
+++ Java4FinalProject/src/main/java/com/example/java4finalproject/Java4FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.java4finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java4FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java4FinalProjectApplication.class, args);
+    }
+
+}
---
+++ Java4FinalProject/src/test/java/com/example/java4finalproject/Java4FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java4finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
